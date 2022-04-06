fa7cff7af811070ef7e3e0be33124374d198d791
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-04-06 19:16:09 +0200

    ejercicio 8B

--- FederacionLuisCuevaEVAL3/src/main/PrincipalExam10.java
+++ FederacionLuisCuevaEVAL3/src/main/PrincipalExam10.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -11,7 +12,10 @@ public class PrincipalExam10 {
 		// TODO Auto-generated method stub
 		Collection<Patrocinador> patrocinadores = new ArrayList();
 		
-		
+		patrocinadores.add(new Patrocinador(1, "ALSA", 500.00, "www.alsa.es", 1, "902422202", LocalTime.of(0, 0), LocalTime.of(23, 59), 1011));
+		patrocinadores.add(new Patrocinador(2, "Ayto. Gijón", 250.00, "www.gijon.es", 2, "985181105", LocalTime.of(9, 0), LocalTime.of(18, 0), 1012));
+		patrocinadores.add(new Patrocinador(3, "Universidad de Oviedo", 350.00, "www.uniovi.es", 3, "985103000", LocalTime.of(8, 30), LocalTime.of(20, 0), 1013));
+		patrocinadores.add(new Patrocinador(4, "CIFP La Laboral", 255.99, "www.cifplalaboral.es", 4, "985185503", LocalTime.of(8, 30), LocalTime.of(18, 0), 1014));
 	}
 
 }
