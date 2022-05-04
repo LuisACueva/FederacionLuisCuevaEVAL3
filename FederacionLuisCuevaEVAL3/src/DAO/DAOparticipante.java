0a75f8ba3bbc91bb9277b6ae1ac2fd48b3fdc37a
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-05-04 19:46:30 +0200

    participante insertar sin id

--- FederacionLuisCuevaEVAL3/src/DAO/DAOparticipante.java
+++ FederacionLuisCuevaEVAL3/src/DAO/DAOparticipante.java
@@ -49,8 +49,29 @@ public class DAOparticipante implements operacionesCRUD<Participante> {
 
 	@Override
 	public long insertarSinID(Participante elemento) {
-		// TODO Auto-generated method stub
-		return 0;
+		long ret = -1;
+
+		String consultaInsertStr1 = "insert into participante(dorsal, calle, tiempo, penalizacion, otros) values (?,?,?,?,?)";
+		
+		try {
+			if (this.conex == null || this.conex.isClosed())
+				this.conex = ConexBD.establecerConexion();
+			PreparedStatement pstmt = conex.prepareStatement(consultaInsertStr1);
+			pstmt.setLong(1, elemento.getDorsal());
+			pstmt.setString(2, elemento.getCalle()+"");
+			Time tiempo = Time.valueOf(LocalTime.of(elemento.getTiempo().getHoras(), elemento.getTiempo().getMinutos(), elemento.getTiempo().getSegundos(), elemento.getTiempo().getCentesimas()*10000000));
+			pstmt.setTime(3, tiempo);
+			pstmt.setBoolean(4, elemento.isPenalizacion());
+			pstmt.setString(5, elemento.getOtros());
+			int resultadoInsercion = pstmt.executeUpdate();
+			ret = elemento.getId();
+		} catch (SQLException e) {
+			System.out.println("Se ha producido una SQLException:" + e.getMessage());
+			e.printStackTrace();
+			
+		}
+
+		return ret;
 	}
 
 	@Override
