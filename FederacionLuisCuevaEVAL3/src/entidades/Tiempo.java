d1e33a443ce9fe1f156b4b698d3b1baa32a036b8
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-05-04 18:41:05 +0200

    clase tiempo

---
+++ FederacionLuisCuevaEVAL3/src/entidades/Tiempo.java
@@ -0,0 +1,58 @@
+package entidades;
+
+public class Tiempo {
+	
+	private int horas;
+	private int minutos;
+	private int segundos;
+	private int centesimas;
+	
+	public Tiempo() {
+		
+	}
+	
+	public Tiempo(int h, int m, int s, int c) {
+		horas = h;
+		minutos = m;
+		segundos = s;
+		centesimas = c;
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
+
+	public int getCentesimas() {
+		return centesimas;
+	}
+
+	public void setCentesimas(int centesimas) {
+		this.centesimas = centesimas;
+	}
+
+	@Override
+	public String toString() {
+		return horas+":"+minutos+":"+segundos+","+centesimas;
+	}
+	
+}
