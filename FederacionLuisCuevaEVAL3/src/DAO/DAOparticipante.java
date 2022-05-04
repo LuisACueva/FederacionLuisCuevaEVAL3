56a687761a772b294fb8c6c837f046413dd013de
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-05-04 19:58:46 +0200

    participantes buscar por id

--- FederacionLuisCuevaEVAL3/src/DAO/DAOparticipante.java
+++ FederacionLuisCuevaEVAL3/src/DAO/DAOparticipante.java
@@ -2,14 +2,19 @@ package DAO;
 
 import entidades.operacionesCRUD;
 import utils.ConexBD;
+import utils.Datos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.time.LocalTime;
 
+import entidades.Atleta;
+import entidades.DatosPersona;
 import entidades.Participante;
+import entidades.Tiempo;
 
 public class DAOparticipante implements operacionesCRUD<Participante> {
 
@@ -76,8 +81,37 @@ public class DAOparticipante implements operacionesCRUD<Participante> {
 
 	@Override
 	public Participante buscarPorID(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Participante ret = null;
+		String consultaInsertStr = "select * FROM participante WHERE id=?";
+		try {
+			if (this.conex == null || this.conex.isClosed())
+				this.conex = ConexBD.establecerConexion();
+			PreparedStatement pstmt = conex.prepareStatement(consultaInsertStr);
+			pstmt.setLong(1, id);
+			ResultSet result = pstmt.executeQuery();
+			while (result.next()) {
+				long idBD = result.getLong("id");
+				int dorsal = result.getInt("dorsal");
+				char calle = result.getString("calle").toCharArray()[0]; //// puede ser null
+				int horas = result.getTime("tiempo").toLocalTime().getHour();
+				int minutos = result.getTime("tiempo").toLocalTime().getMinute();
+				int segundos = result.getTime("tiempo").toLocalTime().getSecond();
+				int centesimas = result.getTime("tiempo").toLocalTime().getNano()*10000000;
+				Tiempo tiempo = new Tiempo(horas, minutos, segundos, centesimas);
+				boolean penalizacion = result.getBoolean("penalizacion");
+				String otros = result.getString("otros");
+				ret = new Participante(idBD, dorsal, calle, tiempo, penalizacion, otros);
+			}
+			if (conex != null)
+				conex.close();
+		} catch (SQLException e) {
+			System.out.println("Se ha producido una SQLException:" + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Se ha producido una Exception:" + e.getMessage());
+			e.printStackTrace();
+		}
+		return ret;
 	}
 
 }
