b77c5f16a0fa380f4f92fbd0cd397005eb56c251
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-05-04 18:57:48 +0200

    clase participante

--- FederacionLuisCuevaEVAL3/src/entidades/Participante.java
+++ FederacionLuisCuevaEVAL3/src/entidades/Participante.java
@@ -1,15 +1,54 @@
 package entidades;
 
+import java.util.Scanner;
+
 public class Participante {
 	protected long id;
 	protected int dorsal; // valor entre 001 y 150
 	protected char calle;
+	private Tiempo tiempo;
+	private boolean penalizacion = false;
+	private String otros;
 
-	public Participante(long id, int dorsal, char calle) {
+	public Participante(long id, int dorsal, char calle, Tiempo tie, boolean pen, String otr) {
 		super();
 		this.id = id;
 		this.dorsal = dorsal;
 		this.calle = calle;
+		this.tiempo = tie;
+		this.penalizacion = pen;
+		if (penalizacion)
+			this.otros = otr;
+	}
+
+	public Tiempo getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(Tiempo tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	public boolean isPenalizacion() {
+		return penalizacion;
+	}
+
+	public void setPenalizacion(boolean penalizacion) {
+		this.penalizacion = penalizacion;
+		Scanner sc = new Scanner(System.in);
+		if (penalizacion) {
+			this.otros = sc.next();
+		}else {
+			this.otros = null;
+		}
+	}
+
+	public String getOtros() {
+		return otros;
+	}
+
+	public void setOtros(String otros) {
+		this.otros = otros;
 	}
 
 	public Participante() {
@@ -39,4 +78,9 @@ public class Participante {
 		this.calle = calle;
 	}
 
+	@Override
+	public String toString() {
+		return "Participante [id=" + id + ", dorsal=" + dorsal + ", calle=" + calle + ", tiempo=" + tiempo
+				+ ", penalizacion=" + penalizacion + ", otros=" + otros + "]";
+	}
 }
