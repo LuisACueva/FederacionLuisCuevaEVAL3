2a16549bb41a058e82523f23f9dbf36db4b0eb46
usut01 <usut01@A204PC03.desarrolloweb.local>
2022-05-18 18:02:43 +0200

    diseno cerrar prueba

--- FederacionLuisCuevaEVAL3/src/GUI/CerrarPrueba.java
+++ FederacionLuisCuevaEVAL3/src/GUI/CerrarPrueba.java
@@ -37,6 +37,8 @@ import java.time.LocalDate;
 import java.awt.event.ActionEvent;
 import javax.swing.border.EtchedBorder;
 import java.awt.Color;
+import javax.swing.JCheckBox;
+import javax.swing.SpinnerNumberModel;
 
 public class CerrarPrueba extends JFrame {
 
@@ -78,7 +80,7 @@ public class CerrarPrueba extends JFrame {
 		else
 		setTitle("Cerrar Prueba INDETERMINADA");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 453, 478);
+		setBounds(100, 100, 453, 551);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -154,15 +156,15 @@ public class CerrarPrueba extends JFrame {
 		panel_1.add(lblPatrocinador);
 
 		JLabel lblprimerpuesto = new JLabel("Primer puesto *:");
-		lblprimerpuesto.setBounds(27, 251, 109, 14);
+		lblprimerpuesto.setBounds(27, 224, 109, 14);
 		contentPane.add(lblprimerpuesto);
 
 		JLabel lblsegundopuesto = new JLabel("Segundo puesto *:");
-		lblsegundopuesto.setBounds(27, 282, 109, 14);
+		lblsegundopuesto.setBounds(27, 314, 109, 14);
 		contentPane.add(lblsegundopuesto);
 
 		JLabel lbltercerpuesto = new JLabel("Tercer puesto *:");
-		lbltercerpuesto.setBounds(27, 313, 109, 14);
+		lbltercerpuesto.setBounds(27, 394, 109, 14);
 		contentPane.add(lbltercerpuesto);
 
 		DefaultComboBoxModel<Atleta> atletasModel = new DefaultComboBoxModel<Atleta>();
@@ -178,19 +180,19 @@ public class CerrarPrueba extends JFrame {
 		JComboBox<Atleta> comboBoxPuesto1 = new JComboBox<Atleta>();
 		comboBoxPuesto1.setModel(new DefaultComboBoxModel(atletasStr));		
 		lblprimerpuesto.setLabelFor(comboBoxPuesto1);
-		comboBoxPuesto1.setBounds(140, 247, 287, 22);
+		comboBoxPuesto1.setBounds(140, 220, 287, 22);
 		contentPane.add(comboBoxPuesto1);
 
 		JComboBox<Atleta> comboBoxPuesto2 = new JComboBox<Atleta>();
 		comboBoxPuesto2.setModel(new DefaultComboBoxModel(atletasStr));
 		lblsegundopuesto.setLabelFor(comboBoxPuesto2);
-		comboBoxPuesto2.setBounds(140, 278, 287, 22);
+		comboBoxPuesto2.setBounds(140, 310, 287, 22);
 		contentPane.add(comboBoxPuesto2);
 
 		JComboBox<Atleta> comboBoxPuesto3 = new JComboBox<Atleta>();
 		comboBoxPuesto3.setModel(new DefaultComboBoxModel(atletasStr));
 		lbltercerpuesto.setLabelFor(comboBoxPuesto3);
-		comboBoxPuesto3.setBounds(140, 309, 287, 22);
+		comboBoxPuesto3.setBounds(140, 390, 287, 22);
 		contentPane.add(comboBoxPuesto3);
 
 		LocalDate hoyMas1MesLD = LocalDate.now().plusMonths(1);
@@ -317,7 +319,7 @@ public class CerrarPrueba extends JFrame {
 				}
 			}
 		});
-		buttonAceptar.setBounds(230, 405, 89, 23);
+		buttonAceptar.setBounds(233, 478, 89, 23);
 		contentPane.add(buttonAceptar);
 
 		JButton buttonCancelar = new JButton("Cancelar");
@@ -333,8 +335,149 @@ public class CerrarPrueba extends JFrame {
 
 			}
 		});
-		buttonCancelar.setBounds(335, 405, 89, 23);
+		buttonCancelar.setBounds(338, 478, 89, 23);
 		contentPane.add(buttonCancelar);
+		
+		JLabel lblDefinitivo = new JLabel("Establecer como DEFINITIVO:");
+		lblDefinitivo.setBounds(27, 482, 153, 14);
+		contentPane.add(lblDefinitivo);
+		
+		JCheckBox chckbxDefinitivo = new JCheckBox("");
+		lblDefinitivo.setLabelFor(chckbxDefinitivo);
+		chckbxDefinitivo.setBounds(172, 478, 21, 23);
+		contentPane.add(chckbxDefinitivo);
+		
+		JLabel lblIdOro = new JLabel("id Oro:");
+		lblIdOro.setBounds(90, 285, 46, 14);
+		contentPane.add(lblIdOro);
+		
+		JLabel lblIdPlata = new JLabel("id Plata:");
+		lblIdPlata.setBounds(90, 369, 46, 14);
+		contentPane.add(lblIdPlata);
+		
+		JLabel lblIdBronce = new JLabel("id Bronce:");
+		lblIdBronce.setBounds(79, 454, 57, 14);
+		contentPane.add(lblIdBronce);
+		
+		JComboBox comboBoxOros = new JComboBox();
+		comboBoxOros.setBounds(140, 281, 97, 22);
+		contentPane.add(comboBoxOros);
+		
+		JComboBox comboBoxPlatas = new JComboBox();
+		comboBoxPlatas.setBounds(140, 365, 97, 22);
+		contentPane.add(comboBoxPlatas);
+		
+		JComboBox comboBoxBronces = new JComboBox();
+		comboBoxBronces.setBounds(140, 450, 97, 22);
+		contentPane.add(comboBoxBronces);
+		
+		JLabel lblHoras_1 = new JLabel("Horas:");
+		lblHoras_1.setBounds(37, 249, 46, 14);
+		contentPane.add(lblHoras_1);
+		
+		JLabel lblHoras_2 = new JLabel("Horas:");
+		lblHoras_2.setBounds(37, 339, 46, 14);
+		contentPane.add(lblHoras_2);
+		
+		JLabel lblHoras_3 = new JLabel("Horas:");
+		lblHoras_3.setBounds(37, 419, 46, 14);
+		contentPane.add(lblHoras_3);
+		
+		JSpinner spinnerH1 = new JSpinner();
+		lblHoras_1.setLabelFor(spinnerH1);
+		spinnerH1.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
+		spinnerH1.setBounds(73, 249, 41, 20);
+		contentPane.add(spinnerH1);
+		
+		JLabel lblMinutos_1 = new JLabel("Minutos:");
+		lblMinutos_1.setBounds(124, 249, 46, 14);
+		contentPane.add(lblMinutos_1);
+		
+		JSpinner spinnerM1 = new JSpinner();
+		lblMinutos_1.setLabelFor(spinnerM1);
+		spinnerM1.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+		spinnerM1.setBounds(174, 249, 41, 20);
+		contentPane.add(spinnerM1);
+		
+		JLabel lblSegundos_1 = new JLabel("Segundos:");
+		lblSegundos_1.setBounds(225, 249, 66, 14);
+		contentPane.add(lblSegundos_1);
+		
+		JSpinner spinnerS1 = new JSpinner();
+		lblSegundos_1.setLabelFor(spinnerS1);
+		spinnerS1.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+		spinnerS1.setBounds(281, 249, 41, 20);
+		contentPane.add(spinnerS1);
+		
+		JLabel lblCentesimas_1 = new JLabel("Centesimas:");
+		lblCentesimas_1.setBounds(324, 249, 72, 14);
+		contentPane.add(lblCentesimas_1);
+		
+		JSpinner spinnerC1 = new JSpinner();
+		lblCentesimas_1.setLabelFor(spinnerC1);
+		spinnerC1.setModel(new SpinnerNumberModel(0, 0, 99, 1));
+		spinnerC1.setBounds(386, 249, 41, 20);
+		contentPane.add(spinnerC1);
+		
+		JSpinner spinnerH2 = new JSpinner();
+		lblHoras_2.setLabelFor(spinnerH2);
+		spinnerH2.setBounds(73, 338, 41, 20);
+		contentPane.add(spinnerH2);
+		
+		JLabel lblMinutos_1_1 = new JLabel("Minutos:");
+		lblMinutos_1_1.setBounds(124, 338, 46, 14);
+		contentPane.add(lblMinutos_1_1);
+		
+		JSpinner spinnerM2 = new JSpinner();
+		lblMinutos_1_1.setLabelFor(spinnerM2);
+		spinnerM2.setBounds(174, 338, 41, 20);
+		contentPane.add(spinnerM2);
+		
+		JLabel lblSegundos_1_1 = new JLabel("Segundos:");
+		lblSegundos_1_1.setBounds(225, 338, 66, 14);
+		contentPane.add(lblSegundos_1_1);
+		
+		JSpinner spinnerS2 = new JSpinner();
+		lblSegundos_1_1.setLabelFor(spinnerS2);
+		spinnerS2.setBounds(281, 338, 41, 20);
+		contentPane.add(spinnerS2);
+		
+		JLabel lblCentesimas_1_1 = new JLabel("Centesimas:");
+		lblCentesimas_1_1.setBounds(324, 338, 72, 14);
+		contentPane.add(lblCentesimas_1_1);
+		
+		JSpinner spinnerC2 = new JSpinner();
+		lblCentesimas_1_1.setLabelFor(spinnerC2);
+		spinnerC2.setBounds(386, 338, 41, 20);
+		contentPane.add(spinnerC2);
+		
+		JSpinner spinnerH3 = new JSpinner();
+		spinnerH3.setBounds(73, 419, 41, 20);
+		contentPane.add(spinnerH3);
+		
+		JLabel lblMinutos_1_2 = new JLabel("Minutos:");
+		lblMinutos_1_2.setBounds(124, 419, 46, 14);
+		contentPane.add(lblMinutos_1_2);
+		
+		JSpinner spinnerM3 = new JSpinner();
+		spinnerM3.setBounds(174, 419, 41, 20);
+		contentPane.add(spinnerM3);
+		
+		JLabel lblSegundos_1_2 = new JLabel("Segundos:");
+		lblSegundos_1_2.setBounds(225, 419, 66, 14);
+		contentPane.add(lblSegundos_1_2);
+		
+		JSpinner spinnerS3 = new JSpinner();
+		spinnerS3.setBounds(281, 419, 41, 20);
+		contentPane.add(spinnerS3);
+		
+		JLabel lblCentesimas_1_2 = new JLabel("Centesimas:");
+		lblCentesimas_1_2.setBounds(324, 419, 72, 14);
+		contentPane.add(lblCentesimas_1_2);
+		
+		JSpinner spinnerC3 = new JSpinner();
+		spinnerC3.setBounds(386, 419, 41, 20);
+		contentPane.add(spinnerC3);
 
 	}
 }
